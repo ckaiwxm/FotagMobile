768dcd0153be591ab83304acc7e03ec556e241d3
dongwoo1005 <Ehddnths93>
2016-03-18 05:00:34 -0400

    Initial commit

---
+++ app/src/test/java/io/github/dongwoo1005/fotagmobile/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package io.github.dongwoo1005.fotagmobile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
